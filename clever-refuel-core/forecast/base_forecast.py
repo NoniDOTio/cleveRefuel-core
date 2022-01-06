261f82e967cd4c0c750a3c581aca3d70176d2652
Arne Perschke <arne.perschke@web.de>
2022-01-06 11:02:46 +0100

    Added Documentation to base_forecasting

--- clever-refuel-core/forecast/base_forecast.py
+++ clever-refuel-core/forecast/base_forecast.py
@@ -2,10 +2,18 @@ from abc import abstractmethod
 from data_reader import DataReader
 from model.tank_stop import TankStop
 
+"""
+Ist die Grundlage aller Forecasting Klassen. Forecasting Klassen ermoeglichen
+das abschaetzen von Spritpreisen.
+"""
 class BaseForecast:
     def __init__(self) -> None:
         self.data_reader = DataReader()
 
+    """
+    Versucht den Preis fuer den gegebenen Tankstop abzuschaetzen. Der Preis wird
+    in ct zurueckgegeben.
+    """
     @abstractmethod
     def get_forecast_for(self, fuel_stop: TankStop) -> int:
         pass
