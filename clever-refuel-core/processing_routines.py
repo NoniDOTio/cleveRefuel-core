7852649fcd9d9c2438b0bb6bc773577f1a6f99b1
Arne Perschke <arne.perschke@web.de>
2022-01-06 10:50:32 +0100

    Added doc zur Processing Routine

--- clever-refuel-core/processing_routines.py
+++ clever-refuel-core/processing_routines.py
@@ -7,12 +7,20 @@ from forecast.brandwide_forecast import BrandwideForecasts
 from calculate_gas_usage.fixed_path_gas_station_problem_algorithm import *
 from calculate_gas_usage.naive import *
 
-
+"""
+Grundlage fuer alle Processing Typen. Jeder Processing type stellt einen
+Menupunkt dar der ausgefuehrt werden kann.
+"""
 class BaseProcessingType:
     @abstractmethod
     def run(route_data : RouteData) -> bool:
         return False
 
+"""
+Analysiert die vom Nutzer gegebene Route mit einem Naiven Forecast auf naive
+weise. hierbei wird der Durchschnittspreis der Tankstelle immer getankt wenn der
+Tank nicht mehr bis zum naechsten Stop reicht
+"""
 class AnalyzeWithNaiveForecastOnNaiveRoute(BaseProcessingType):
 
     def run(route_data: RouteData) -> bool:
@@ -21,6 +29,11 @@ class AnalyzeWithNaiveForecastOnNaiveRoute(BaseProcessingType):
         calculate_naively(route_data, forecast)
         return True
 
+"""
+Analysiert die vom Nutzer gegebene Route mit einem Brandwide Forecast auf naive
+weise. hierbei wird der Durchschnittspreis der Marke immer getankt wenn der
+Tank nicht mehr bis zum naechsten Stop reicht
+"""
 class AnalyzeWithBrandwideForecastOnNaiveRoute(BaseProcessingType):
 
     def run(route_data: RouteData) -> bool:
@@ -29,6 +42,11 @@ class AnalyzeWithBrandwideForecastOnNaiveRoute(BaseProcessingType):
         calculate_naively(route_data, forecast)
         return True
 
+"""
+Analysiert die vom Nutzer gegebene Route mit einem Naiven Forecast mithilfe
+des Fixed path gas station problem algorithm. Hierbei wird der Durchschnittspreis
+der Tankstelle an den optimalen Tankstellen getankt.
+"""
 class AnalyzeWithFixedPathGasStationProblem(BaseProcessingType):
 
     def run(route_data: RouteData) -> bool:
@@ -37,6 +55,11 @@ class AnalyzeWithFixedPathGasStationProblem(BaseProcessingType):
         calculate_using_fixed_path_gas_station_problem_algorithm(route_data, forecast)
         return True
 
+"""
+Analysiert die vom Nutzer gegebene Route mit einem Naiven Forecast mithilfe
+des Fixed path gas station problem algorithm. Hierbei wird der Durchschnittspreis
+der Marke an den optimalen Tankstellen getankt.
+"""
 class AnalyzeBrandwideWithFixedPathGasStationProblem(BaseProcessingType):
 
     def run(route_data: RouteData) -> bool:
